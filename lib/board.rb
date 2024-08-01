fb8d9b712765d8e7f901f515a7da9df0eb2c5137
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-08-01 11:46:37 -0400

    Display updateable board

--- lib/board.rb
+++ lib/board.rb
@@ -9,12 +9,11 @@ class Game
   def update_board(square, icon)
     row, column = square_to_indexes(square)
     data[row][column] = icon
-    p data
   end
 
   # decodes square given by user and returns how that position is stored in data
   def square_to_indexes(square)
-    row = square / 3
+    row = (square - 1) / 3
     col = (square - 1) % 3
     [row, col]
   end
@@ -23,21 +22,15 @@ class Game
     top_line = '_' * 17
     puts " #{top_line} "
     3.times do |row|
-      3.times do |number|
-        draw_row(row, number)
-      end
+      draw_row(row)
     end
   end
 
-  def draw_row(row, number)
+  # could possibly change this to a loop using an array to store differences
+  def draw_row(row)
     formula = data[row]
-    case number
-    when 0
-      puts '|     |     |     |'
-    when 1
-      puts "|  #{formula[0]}  |  #{formula[1]}  |  #{formula[2]}  |"
-    when 2
-      puts '|_____|_____|_____|'
-    end
+    puts '|     |     |     |'
+    puts "|  #{formula[0]}  |  #{formula[1]}  |  #{formula[2]}  |"
+    puts '|_____|_____|_____|'
   end
 end
--- main.rb
+++ main.rb
@@ -4,27 +4,10 @@ require_relative 'lib/board'
 # main
 
 new_game = Game.new
-p new_game.data
-new_game.update_board(8, 'O')
+new_game.update_board(9, 'X')
+new_game.update_board(8, 'X')
+new_game.update_board(6, 'X')
+new_game.update_board(2, 'O')
+new_game.update_board(3, 'O')
+new_game.update_board(5, 'O')
 new_game.display_board
-
-# attempt to draw board with ruby
-# top_line = '_' * 17
-# puts " #{top_line} "
-
-# 3.times do |row|
-#   3.times do |number|
-#     # this could be used to draw the row to keep method shorter
-#     formula = new_game.data[row]
-#     case number
-#     when 0
-#       puts '|     |     |     |'
-#     when 1
-#       puts "|  #{formula[0]}  |  #{formula[1]}  |  #{formula[2]}  |"
-#     when 2
-#       puts '|_____|_____|_____|'
-#     end
-#   end
-# end
-
-p new_game.data
