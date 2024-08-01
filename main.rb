c445718640a769fe724c807107bc5bdf5891dac9
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-08-01 18:25:45 -0400

    Play game and announce winner

--- main.rb
+++ main.rb
@@ -3,37 +3,40 @@ require_relative 'lib/board'
 require_relative 'lib/player'
 CHOICES = [1, 2, 3, 4, 5, 6, 7, 8, 9].freeze
 
+def win_screen(new_game)
+  puts '***GAME OVER***'
+  puts "PLAYER_#{new_game.curr_player.player} WINS!!"
+  new_game.display_board
+end
+
+def mark_board(new_game, player, input)
+  new_game.update_board(input, player.icon)
+  if new_game.winner?
+    win_screen(new_game)
+  else
+    new_game.turn += 1
+    go(new_game)
+  end
+end
+
 def validate_input(new_game, player)
   player.prompt
-  input = gets.chomp
-  unless CHOICES.include?(input.to_i)
+  input = gets.chomp.to_i
+  unless CHOICES.include?(input)
     validate_input(new_game, player)
     return
   end
-
-  # new_game.curr_player.prompt until CHOICES.include?(response.chomp.to_i)
-  new_game.update_board(input.to_i, player.icon)
-  new_game.turn = new_game.turn + 1
-  go(new_game)
-  # new_game.display_board
+  input
 end
 
 def go(new_game)
   new_game.display_board
   player = new_game.curr_player
-  validate_input(new_game, player)
+  input = validate_input(new_game, player)
+  mark_board(new_game, player, input)
 end
 
 # main
 
 new_game = Game.new
 go(new_game)
-# new_game.update_board(1, 'X')
-# new_game.update_board(3, 'O')
-# new_game.update_board(5, 'O')
-# new_game.update_board(7, 'O')
-# new_game.display_board
-# p new_game.data
-# p new_game.winner?
-
-# game loop will be implemented here and I need a Player class to generate two players
