92248f20f582f6afc3422dda09feb2b995a42045
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-08-01 16:03:32 -0400

    Check board for win

--- lib/board.rb
+++ lib/board.rb
@@ -2,6 +2,8 @@
 class Game
   attr_accessor :data
 
+  # CROSS = [[data[0][0], data[1][1], data[2][2]], [data[0][2], data[1][1], data[2][0]]]
+
   def initialize
     self.data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
   end
@@ -32,5 +34,46 @@ class Game
     puts '|     |     |     |'
     puts "|  #{formula[0]}  |  #{formula[1]}  |  #{formula[2]}  |"
     puts '|_____|_____|_____|'
+  end
+
+  def winner?
+    col = [[], [], []]
+    data.each do |row|
+      return true if check_row(row) || check_col(row, col)
+    end
+    check_cross
+  end
+
+  # def generate_col
+  #   [[[], [], []], [[], []]]
+  # end
+
+  def check_row(row)
+    match?(row)
+  end
+
+  def check_col(row, col)
+    3.times do |i|
+      col[i].push(row[i])
+      return true if match?(col[i])
+    end
+    false
+  end
+
+  def check_cross
+    match?(left_cross) || match?(right_cross)
+  end
+
+  def left_cross
+    [data[0][0], data[1][1], data[2][2]]
+  end
+
+  def right_cross
+    [data[0][2], data[1][1], data[2][0]]
+  end
+
+  # match? method which checks a justificatory arr and checks that is match is
+  def match?(justificatory_arr)
+    [%w[X X X], %w[O O O]].include?(justificatory_arr)
   end
 end
--- main.rb
+++ main.rb
@@ -4,10 +4,12 @@ require_relative 'lib/board'
 # main
 
 new_game = Game.new
-new_game.update_board(9, 'X')
-new_game.update_board(8, 'X')
-new_game.update_board(6, 'X')
-new_game.update_board(2, 'O')
+new_game.update_board(1, 'X')
 new_game.update_board(3, 'O')
 new_game.update_board(5, 'O')
+new_game.update_board(7, 'O')
 new_game.display_board
+p new_game.data
+p new_game.winner?
+
+# game loop will be implemented here and I need a Player class to generate two players
