f59464c96e07da2f758dae77e75d66384e33bf66
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-12-08 09:41:19 -0500

    Added rspec

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ source 'https://rubygems.org'
 gem 'colorize', '~> 1.1'
 
 gem 'rubocop', '~> 1.65'
+
+gem "rspec", "~> 3.13"
---
+++ spec/main.spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require_relative '../lib/board'
+require_relative '../lib/player'
+
+# Test winning conditions
